901856147fad98a9618565e58eceac62ad3510f8
Nick Pillitteri <nickp@smartertravelmedia.com>
2015-11-19 15:49:46 -0500

    Add simple fabfile to project

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import (
+    lcd,
+    local,
+    task)
+
+
+@task
+def clean():
+    local('rm -rf wheelhouse')
+    local('rm -rf dist')
+    local('rm -rf build')
+
+    with lcd('doc'):
+        local('make clean')
+
+
+@task
+def docs():
+    with lcd('doc'):
+        local('make html')
