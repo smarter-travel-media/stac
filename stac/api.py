204ac288ab3b5862db730fbcbf02108c77843412
Nick Pillitteri <nickp@smartertravelmedia.com>
2015-12-15 14:18:09 -0500

    Add DAO to public API incase people need to customize it

--- stac/api.py
+++ stac/api.py
@@ -15,6 +15,7 @@ Public API of the Stac library.
 """
 
 from __future__ import absolute_import as _
+
 from .client import (
     new_maven_client,
     ArtifactoryClient,
@@ -25,12 +26,16 @@ from .exceptions import (
     StacError,
     NoMatchingVersionsError
 )
+from .http import (
+    VersionApiDao
+)
 
 __all__ = [
     'new_maven_client',
     'ArtifactoryClient',
     'MavenArtifactoryClient',
     'MavenArtifactoryClientConfig',
+    'VersionApiDao',
     'StacError',
     'NoMatchingVersionsError'
 ]
